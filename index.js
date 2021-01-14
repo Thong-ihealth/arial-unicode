0ec6b8a9944a7dc539c908f6f4009b52ad7444f6
Thong Le <thong.le@ihealthlabs.com>
2021-01-14 06:52:11 -0800

    Add tool to convert fonts
    Added Fontmin
    Test conversion to woff

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 console.log('Hello, Arial Unicode MS');
 
 // git tag v0.1
+
+// convert fonts
+const Fontmin = require('fontmin');
+
+const filename = 'Arial-Unicode-Bold.ttf';
+
+const fontmin = new Fontmin()
+                  .src(filename)
+                  .dest('build')
+                  .use(Fontmin.ttf2woff({
+                    deflate: true
+                  }));
+
+console.log('converting');
+fontmin.run((err, files) => {
+  if (err) throw err;
+
+  console.log('converted ', files);
+});
